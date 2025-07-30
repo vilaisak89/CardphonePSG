fdf427ea23347e582522e9a000386d5d767c7d42
vilaisak89 <vilaisak.s@phonesackgroup.com>
2025-07-30 14:14:16 +0700

    Create project 01

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CardPhonePSG</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Laravel2</h1>
+    <hr>
+    <div id="app-vue"></div>
+    @vite('resources/js/app.js')
+</body>
+</html>
